994da174c137bb8479c2976ec3dfdebbe79372b8
YBoah <yawboahene.yb@gmail.com>
2024-03-05 23:16:56 +0000

    Adding movie search functionality using OMDB API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=1a574970";
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Antman");
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
     </div>
   );
-}
+};
 
 export default App;
