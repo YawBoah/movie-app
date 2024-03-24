37bad664401c18970a9b3feb98570375b870283e
YBoah <yawboahene.yb@gmail.com>
2024-03-24 23:57:21 +0000

    More changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Justice League");
   }, []);
 
   const searchMovies = async (title) => {
